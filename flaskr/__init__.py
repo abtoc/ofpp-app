a60552d4cc56b32b4babac59c3981239d691a398
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-09-12 17:49:51 +0900

    Hello Workdを実装

---
+++ flaskr/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('flaskr.config')
+app.config.from_pyfile('config.py', silent=True)
+app.jinja_env.add_extension('pypugjs.ext.jinja.PyPugJSExtension')
+
+import flaskr.views
