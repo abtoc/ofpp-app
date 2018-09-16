f1a4f1a545de8cf62cb1781eb3917ca56206569b
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-09-16 10:22:05 +0900

    受給者証期限管理を実装

--- flaskr/services/recipients.py
+++ flaskr/services/recipients.py
@@ -1,3 +1,5 @@
+from datetime import date
+from dateutil.relativedelta import relativedelta
 from flask import abort
 from flaskr import db
 from flaskr.services.persons import PersonService
@@ -8,6 +10,18 @@ class RecipientService(Recipient):
         form.populate_obj(self)
         db.session.add(self)
         db.session.commit()
+    def is_supply_over(self):
+        if not bool(self.supply_out):
+            return False
+        dead = date.today()
+        dead += relativedelta(months=1)
+        return self.supply_out < dead     
+    def is_apply_over(self):
+        if not bool(self.apply_out):
+            return False
+        dead = date.today()
+        dead += relativedelta(months=1)
+        return self.apply_out < dead     
     @classmethod
     def get_or_404(cls, id):
         result = cls.query.get(id)
