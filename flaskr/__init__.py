1c06c463135ec621e9bb84c63e69046bc60ecd77
Toshio Abe@pc01.local <abtoc88@gmail.com>
2018-09-17 20:15:02 +0900

    workerが呼び出されないため、make_celeryの場所を変えた

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,11 +1,23 @@
+from celery import Celery
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.contrib.cache import SimpleCache
-from flaskr.workers import CeleryX
 import pymysql
 pymysql.install_as_MySQLdb
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('flaskr.config')
@@ -22,11 +34,11 @@ auth = HTTPBasicAuth()
 
 cache = SimpleCache()
 
-celery = CeleryX(app)
-celery.conf.update(app.config)
+celery = make_celery(app)
 
 import flaskr.models
 import flaskr.helpers
 import flaskr.views
 import flaskr.apis
 import flaskr.reports
+import flaskr.workers
--- flaskr/workers/__init__.py
+++ flaskr/workers/__init__.py
@@ -1,14 +1,2 @@
-from celery import Celery
-
-class CeleryX(Celery):
-    def __init__(self, app):
-        super().__init__(
-            backend=app.config['CELERY_RESULT_BACKEND'],
-            broker=app.config['CELERY_BROKER_URL']
-        )
-        class ContextTask(self.Task):
-            abstract = True
-            def __call__(self, *args, **kwargs):
-                with app.app_context():
-                    return self.run(*args, **kwargs)
-        self.Task = ContextTask
+from flaskr.workers import worklogs
+from flaskr.workers import performlogs
