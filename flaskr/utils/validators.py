ae70ecbbfbcc7f397aa3f6a68f227234ee5680dd
Toshio Abe@pc03 <aboc88@gmail.com>
2018-09-18 12:03:10 +0900

    時刻のvalidator強化

--- flaskr/utils/validators.py
+++ flaskr/utils/validators.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from wtforms import ValidationError
 from wtforms.validators import Regexp
 
@@ -8,4 +9,9 @@ class WorkTime(Regexp):
         if 'regex' not in kwargs:
             kwargs['regex'] = '^[0-2][0-9]:[0-5][0-9]$'
         super().__init__(*args, **kwargs)
-
+    def __call__(self, form, field):
+        super().__call__(form, field)
+        try:
+            dt = datetime.strptime(field.data, '%H:%M')
+        except ValueError:
+            raise ValidationError('HH:MMの形式で入力してください')
